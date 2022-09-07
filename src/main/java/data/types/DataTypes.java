9ebe70cd725c6e5762005ea3d9673bb9e90f037d
allexkz <alex.adriel@gmail.com>
2022-09-06 22:49:48 -0300

    second assignment about data types

---
+++ src/main/java/data/types/DataTypes.java
@@ -0,0 +1,45 @@
+package data.types;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        /* Primitives Data Types */
+
+        byte b = -17;
+        byte e = 126;
+//        byte b = -129; byte types just store from -128 to 128
+//        byte b = 129; same
+
+        short x = -31748;
+        short v = 31748;
+//        short x = -40000; short types range are -32,768 to 32,767
+//        short x = 40000; same
+
+        int po = 214748364;
+        int pa = -214748364;
+//        integers store range are -2,147,483,648 to 2,147,483,647
+
+        long la = 92233565456132L;
+        long lp = -92233112313123L;
+//        long store range are -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//        long data type need the usage of "L"
+
+        float fl = 5.47F;
+        float fx = -5.47F;
+//        float allows to store fractional numbers, store 6 to 7 decimal digits
+
+        double da = 5.40000000000000D;
+        double dx = -5.40000000000000D;
+//        double allow storing fractional numbers too, range are 15 decimal digits
+
+        boolean ap = true;
+        boolean ax = false;
+//        boolean stores "true" or "false" conditions
+
+        char op = 'p';
+        char ok = '\u0134';
+//        stores a single letter/character or unicode/ASCII
+
+
+    }
+}
